c439f2f50e144eb4f296a813578b34141fc69e11
imeian <imeian@gmail.com>
2019-09-19 13:57:22 +0200

    unique db connection

--- init-db.js
+++ init-db.js
@@ -5,11 +5,11 @@ const MongoClient = require("mongodb").MongoClient
 
 // "mongodb://localhost:27017"
 MongoClient.connect("mongodb+srv://g3k:6VSa7J6pTCsNEfyC@cluster0-jge1t.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
-.then(client => queryDb(client.db('game-3000')), {
+.then(client => queryDb(client.db('game-3000'), {
     collection: 'problems',
     type: 'insertMany',
     filter: problems,
     callback: () => {
         console.log('Successfully populated database')
         process.exit()
-    }})
+    }}))
