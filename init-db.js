0d238edc0a43a3525183362ad12310c16d55e3dd
imeian <imeian@gmail.com>
2019-09-19 13:51:44 +0200

    dep-4

--- init-db.js
+++ init-db.js
@@ -1,13 +1,15 @@
 const queryDb = require("./methods/queryDb")
 const problems = require("./database/problems")
 
-queryDb({
+const MongoClient = require("mongodb").MongoClient
+
+// "mongodb://localhost:27017"
+MongoClient.connect("mongodb+srv://g3k:6VSa7J6pTCsNEfyC@cluster0-jge1t.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
+.then(client => queryDb(client.db('game-3000')), {
     collection: 'problems',
     type: 'insertMany',
     filter: problems,
     callback: () => {
         console.log('Successfully populated database')
         process.exit()
-    }
-})
-
+    }})
