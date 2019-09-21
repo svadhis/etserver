59ddad3a2d13164a242a43dbc9c4253931f7e306
Svadhis <imeian@gmail.com>
2019-09-22 00:18:19 +0200

    22.9-4

--- database/config/db.js
+++ database/config/db.js
@@ -1,8 +1,8 @@
 // Set db here
 
 const db = {
-    uri: '',
-    name: ''
+    uri: 'mongodb+srv://g3k:6VSa7J6pTCsNEfyC@cluster0-jge1t.mongodb.net/test?retryWrites=true&w=majority',
+    name: 'game-3000'
 }
 
 module.exports = db
